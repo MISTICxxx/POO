3f0e4f470866fdd3f018579b50738db0a8c47e83
Hiago <hiagobernardo2@gmail.com>
2024-08-08 18:32:23 -0300

    Add files via upload
    primeiros objetos

---
+++ Cozinha.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Cozinha {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Fogao fogao1 = new Fogao(4, "eletrolux", "eletrico", "nao", "preto");
+        Fogao fogao2 = new Fogao(4, "brastemp", "gas", "sim", "preto");
+
+        System.out.print("Digite a temperatura do forno: ");
+        fogao1.temperaturaForno = sc.nextInt();
+        System.out.println("A temperatura do seu forno está a " + fogao1.temperaturaForno + " graus Celsius");
+        System.out.println(fogao1.marca + " " + fogao1.cor + " " + fogao1.temForno);
+        System.out.println(fogao2.marca + " " + fogao2.cor + " " + fogao2.alimentacao + " " + fogao2.temForno);
+
+        sc.close();
+    }
+}
